b776c07caec60848b79b70ac05111b51406e6733
Opilane TTHK <opilane@tthk.ee>
2020-11-13 09:21:00 +0200

    lol

---
+++ Friday/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace Friday
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            WriteFilePaths();
+
+        }
+
+
+        public static void WriteFilePaths()
+        {
+            string filePath = @"C:\Users\opilane\friday\samplefiles.txt";
+            string directorypathA = @"C:\Users\opilane\friday\sampledirectories.txt";
+            string directoryPath = @"C:\Users\opilane\samples";
+
+            string[] filesA = Directory.GetFiles(directoryPath, "*", SearchOption.AllDirectories);
+            string[] filesB = Directory.GetFiles(directoryPath, "*.*", SearchOption.AllDirectories);
+
+            File.WriteAllLines(directorypathA, filesA);
+            File.WriteAllLines(filePath, filesB);
+        }
+    }
+}
